b65aa098d824ff397f65770251685fe1b8a9d81e
Kerry Lang <klang812@gmail.com>
2021-01-29 09:40:49 -0800

    add a return string to user for their order, test to pass

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,16 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
 }
 
+Pizza.prototype.order = function() {
+  return this.size + " " + this.toppings;
+}
+
 Pizza.prototype.price = function() {
-  basePrice = 10
+  baseSmall = 10;
+  baseMedium = 12;
+  baseLarge = 14;
   let pizzaPrice;
+  this.size = 
   //let toppingQty;
     //for (let index = 1; index<=toppingQty; index++) {
   pizzaPrice = basePrice + (toppingQty * 4)
