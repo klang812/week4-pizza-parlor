74339087f5cd9049e48e4596c54b38648de5b174
Kerry Lang <klang812@gmail.com>
2021-01-29 16:33:32 -0800

    add function to count checkboxes for toppings, need bug fix for jquery order total show

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ function Pizza(size) {
   this.toppings = [];
 }
 
-Pizza.prototype.addTopping = function(topping) {
-  this.toppings.push(topping);
-}
+// Pizza.prototype.addTopping = function(topping) {
+//   this.toppings.push(topping);
+// }
 
-Pizza.prototype.order = function() {
-  return this.size + " " + this.toppings;
+Pizza.prototype.order = function(topping) {
+  return this.size + " " + this.toppings.push(topping);
 }
 
 Pizza.prototype.totalPrice = function() {
@@ -17,7 +17,19 @@ Pizza.prototype.totalPrice = function() {
   baseMedium = 12;
   baseLarge = 14;
   numToppings;
+  console.log("topping");
   for (index = 0; index<=numToppings; index++) {
+    function numToppings()  {
+      let inputElems = document.getElementsByTagName("input"),
+        count = 0;
+    
+        for (let i=0; i<inputElems.length; i++) {       
+        if (inputElems[i].type == "checkbox" && inputElems[i].checked == true){
+              count++;
+          }
+          alert(count);
+        }
+      }
     if (this.size === "small") {
       totalPrice = (numToppings * 2)  + baseSmall
       } else if (this.size === "medium") {
@@ -29,13 +41,29 @@ Pizza.prototype.totalPrice = function() {
     }
   }
 
+  // function checkboxes()  {
+  // let inputElems = document.getElementsByTagName("input"),
+  //   count = 0;
 
+  //   for (let i=0; i<inputElems.length; i++) {       
+  //   if (inputElems[i].type == "checkbox" && inputElems[i].checked == true){
+  //         count++;
+  //     }
+  //     alert(count);
+  //   }
+  // }
+
+// User Logic ---------------
   $(document).ready(function() {
     $("form#contactInfo").submit(function(event) {
       event.preventDefault();
       const customerName = $("input#customerName").val();
+      
       $("form#orderInfo").hide();
       $("form#orderScreen").show();
+      $("#name").text(customerName);
+      //let newOrder = 
+      //$("#ordertotal").text(???);
     });
 
   });
